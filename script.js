6f8c2258c43c7ae7a34c8e022d90542c1f6f88a8
Lucas Luján <lucasnlujan@gmail.com>
2025-10-01 23:50:01 +0000

    sexto commit

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-const input = document.getElementById("input1")
-console.log(input)
-const submit = document.getElementById("submit")
-submit.onclick = (e) =>{
-    console.log(input.value)
-    e.preventDefault()
-}
+const nombre = "Lucas"; //constantes//
+let edad = 21;
+const saludo= `Hola, me llamo ${nombre} y tengo ${edad} años `;
 
+console.log(nombre); //para que aparezca en la consola//
+console.log (edad);
+console.log (saludo);
+
+
+console.log ("1"==1);
+console.log ("1"===1); // primitivos //
+
+const lista = [1, "hola", null];
+lista.length;
+console.log(lista);
+
+function saludar (nombre) {
+    console.log(`hola ${nombre}`);
+    return `hola ${nombre} retorno`;
+
+
+}
+console.log (saludar (`Lucas`));
+saludar (`Lucas`);
